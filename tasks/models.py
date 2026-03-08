5ead60ab0d3d27af722be7714b46e2368f2bdefe
Truphenah <145029217+AbereTruphenah@users.noreply.github.com>
2026-03-08 13:18:07 +0300

    Created first model - Task

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,12 @@
+# IMPORT DJANGO'S DATABASE MODELS MODULE
 from django.db import models
 
-# Create your models here.
+# CREATE A MODEL/TABLE NAMED TASK
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # CONTROL HOW OBJECT APPEARS IN ADMIN PANEL
+    def __str__(self):
+        return self.title
